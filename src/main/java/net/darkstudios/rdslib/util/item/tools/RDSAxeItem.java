7083b66071a9a12effd7b0feab954a0adfa778a9
RealDarkStudios <jmorton@digitalunderworlds.com>
2023-05-28 21:17:02 -0500

    Add RDS tool classes

---
+++ src/main/java/net/darkstudios/rdslib/util/item/tools/RDSAxeItem.java
@@ -0,0 +1,10 @@
+package net.darkstudios.rdslib.util.item.tools;
+
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.Tier;
+
+public class RDSAxeItem extends AxeItem {
+    public RDSAxeItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+}
---
+++ src/main/java/net/darkstudios/rdslib/util/item/tools/RDSHoeItem.java
@@ -0,0 +1,10 @@
+package net.darkstudios.rdslib.util.item.tools;
+
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.Tier;
+
+public class RDSHoeItem extends HoeItem {
+    public RDSHoeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+}
