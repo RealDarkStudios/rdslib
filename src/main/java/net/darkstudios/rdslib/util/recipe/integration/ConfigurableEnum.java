a911cbec74b26ecb88814d8c3f58481636b2146a
RealDarkStudios <jmorton@digitalunderworlds.com>
2023-06-17 17:27:46 -0500

    RDSLib v1.20.X-0.2.2.1 Add EMI boilerplate classes
    Took 12 minutes

---
+++ src/main/java/net/darkstudios/rdslib/util/recipe/integration/ConfigurableEnum.java
@@ -0,0 +1,9 @@
+package net.darkstudios.rdslib.util.recipe.integration;
+
+import net.minecraft.network.chat.Component;
+
+public interface ConfigurableEnum {
+        String getName();
+
+        Component getText();
+}
