5e70b06a4ee374c16c016c96d5c8082250768aba
RealDarkStudios <jmorton@digitalunderworlds.com>
2023-11-01 14:14:07 -0500

    RDSLib v4.0.3.0-rc2 for MC 1.20 - 1.20.1
    - Add fire and smoke particles to AbstractFurnaceLikeBlockV3
    
    Took 27 minutes

--- src/main/java/net/realdarkstudios/rdslib/block/custom/AbstractFurnaceLikeBlockV3.java
+++ src/main/java/net/realdarkstudios/rdslib/block/custom/AbstractFurnaceLikeBlockV3.java
@@ -1,6 +1,11 @@
 package net.realdarkstudios.rdslib.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -21,6 +26,27 @@ public abstract class AbstractFurnaceLikeBlockV3 extends AbstractFurnaceBlock {
         } else {
             this.openContainer(pLevel, pPos, pPlayer);
             return InteractionResult.CONSUME;
+        }
+    }
+
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (pState.getValue(LIT)) {
+            double d0 = (double)pPos.getX() + 0.5D;
+            double d1 = (double)pPos.getY();
+            double d2 = (double)pPos.getZ() + 0.5D;
+            if (pRandom.nextDouble() < 0.1D) {
+                pLevel.playLocalSound(d0, d1, d2, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            Direction direction = pState.getValue(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d3 = 0.52D;
+            double d4 = pRandom.nextDouble() * 0.6D - 0.3D;
+            double d5 = direction$axis == Direction.Axis.X ? (double)direction.getStepX() * 0.52D : d4;
+            double d6 = pRandom.nextDouble() * 6.0D / 16.0D;
+            double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getStepZ() * 0.52D : d4;
+            pLevel.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+            pLevel.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
         }
     }
 }
